aa941baa169095dc77c629d56c71dfdfaa44e4c4
Yhawbee1 <antwibrian82@gmail.com>
2024-05-30 11:13:34 +0000

    Create Page transition

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import TransitionEffect from '@/components/TransitionEffect'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <TransitionEffect  />
+      About Page
+    </>
+  )
+}
+
+export default page
---
+++ src/components/TransitionEffect.jsx
@@ -0,0 +1,58 @@
+"use client"
+import { motion } from "framer-motion";
+
+const TransitionEffect = () => {
+  return (
+    <>
+      <motion.div
+        className="fixed bottom-0 top-0 right-full z-30 h-screen w-screen bg-black"
+        initial={{
+          x: "100%",
+          width: "100%",
+        }}
+        animate={{
+          x: "0%",
+          width: "0%",
+        }}
+        transition={{
+          duration: 0.6,
+          ease: "easeInOut",
+        }}
+      />
+      <motion.div
+        className="fixed bottom-0 top-0 right-full z-20 h-screen w-screen bg-brianGreen"
+        initial={{
+          x: "100%",
+          width: "100%",
+        }}
+        animate={{
+          x: "0%",
+          width: "0%",
+        }}
+        transition={{
+          delay: 0.2,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+      />
+      <motion.div
+        className="fixed bottom-0 top-0 right-full z-10 h-screen w-screen bg-black"
+        initial={{
+          x: "100%",
+          width: "100%",
+        }}
+        animate={{
+          x: "0%",
+          width: "0%",
+        }}
+        transition={{
+          delay: 0.4,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+      />
+    </>
+  );
+};
+
+export default TransitionEffect;
