2819abe107cc65745e7682c7fb57baa3afd293e5
Gyimah05 <bekoepiesie@gmail.com>
2024-08-04 16:09:07 +0000

    Format the code

--- src/app/contact-me/page.jsx
+++ src/app/contact-me/page.jsx
@@ -7,21 +7,17 @@ import { FaXTwitter } from "react-icons/fa6";
 import TransitionEffect from "@/components/TransitionEffect";
 import MouseTracking from "@/components/MouseTracking";
 
-
 export default function Contact() {
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const formRef = useRef(null)
+  const formRef = useRef(null);
 
   async function handleSubmit(event) {
     event.preventDefault();
-    
+
     const formData = new FormData(event.target);
-    formData.append(
-      "access_key",
-      "c197cf33-1230-406f-acf4-eb7c97b8ecfb"
-    );
+    formData.append("access_key", "c197cf33-1230-406f-acf4-eb7c97b8ecfb");
 
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
@@ -46,9 +42,10 @@ export default function Contact() {
           formRef.current.reset(); // Reset the form fields
         }
         setIsLoading(false);
-        
       } else {
-        setError("We encountered an issue while processing your request. Please try again.");
+        setError(
+          "We encountered an issue while processing your request. Please try again.",
+        );
         setSuccess(null); // Clear any previous success messages
         setIsLoading(false);
       }
@@ -61,57 +58,84 @@ export default function Contact() {
 
   return (
     <>
-    <TransitionEffect />
-    <MouseTracking />
-    <section className="flex flex-col h-full items-center justify-center px-3 md:px-5">
-      <div className="text-start">
-        <div className="mb-6 space-y-2">
-          <h1 className="font-epilogue lg:text-[96px] leading-[50px] lg:leading-[75px] text-[50px] md:text-[65px] font-bold w-full">Contact <span className="text-brianGreen">Me.</span></h1>  
-          <p className="font-poppins ps-1">Got a project or question? Reach out, and I&apos;ll get back to you soon.</p>
-          <div className="flex gap-3 ps-1 mt-3 text-xl md:text-2xl">
-            <a href="https://github.com/yhawbee1" className="hover:text-brianGreen"><FaGithub /></a>
-            <a href="https://www.linkedin.com/in/brian-antwi-a7a136264" className="hover:text-brianGreen"><FaLinkedin /></a>
-            <a href="https://x.com/BrianAntwi23?t=mEdh1IedSFx2GRGo2FEpVA&s=09" className="hover:text-brianGreen"><FaXTwitter /></a>
-            <a href="https://www.instagram.com/khelly_spikes?igsh=b2dxODI2MGpwOHhl" className="hover:text-brianGreen"><FaInstagram /></a>
+      <TransitionEffect />
+      <MouseTracking />
+      <section className="flex h-full flex-col items-center justify-center px-3 md:px-5">
+        <div className="text-start">
+          <div className="mb-6 space-y-2">
+            <h1 className="w-full font-epilogue text-[50px] font-bold leading-[50px] md:text-[65px] lg:text-[96px] lg:leading-[75px]">
+              Contact <span className="text-brianGreen">Me.</span>
+            </h1>
+            <p className="ps-1 font-poppins">
+              Got a project or question? Reach out, and I&apos;ll get back to
+              you soon.
+            </p>
+            <div className="mt-3 flex gap-3 ps-1 text-xl md:text-2xl">
+              <a
+                href="https://github.com/yhawbee1"
+                className="hover:text-brianGreen"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/brian-antwi-a7a136264"
+                className="hover:text-brianGreen"
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href="https://x.com/BrianAntwi23?t=mEdh1IedSFx2GRGo2FEpVA&s=09"
+                className="hover:text-brianGreen"
+              >
+                <FaXTwitter />
+              </a>
+              <a
+                href="https://www.instagram.com/khelly_spikes?igsh=b2dxODI2MGpwOHhl"
+                className="hover:text-brianGreen"
+              >
+                <FaInstagram />
+              </a>
+            </div>
           </div>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <div className="space-y-3 md:space-y-4">
+              <input
+                type="text"
+                placeholder="Name"
+                className="w-full rounded-full border border-white bg-black/10 py-5 pe-2 ps-4 font-epilogue backdrop-blur-md placeholder:font-poppins placeholder:text-white focus:border-brianGreen  focus:outline-0 md:text-lg md:placeholder:text-lg "
+                name="name"
+                required
+              />
+              <input
+                type="email"
+                className="w-full rounded-full border border-white bg-black/10 py-5 pe-2 ps-4 font-epilogue backdrop-blur-md placeholder:font-poppins placeholder:text-white focus:border-brianGreen  focus:outline-0 md:text-lg md:placeholder:text-lg "
+                placeholder="Email"
+                name="email"
+                required
+              />
+              <textarea
+                className="h-[150px] w-full rounded-3xl border border-white bg-black/10 py-5 pe-2 ps-4 font-epilogue backdrop-blur-md placeholder:font-poppins placeholder:text-white focus:border-brianGreen focus:outline-0 md:text-lg md:placeholder:text-lg "
+                placeholder="Message"
+                name="message"
+                required
+              ></textarea>
+              <Button
+                className="h-[50px] w-full rounded-full border  border-brianGreen bg-brianGreen font-poppins text-black hover:bg-transparent hover:text-brianGreen max-[320px]:h-fit md:h-[60px] md:w-[250px] 2xl:text-xl"
+                type="submit"
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <Loader className="animate-spin" />
+                ) : (
+                  "Send Message"
+                )}
+              </Button>
+              {success && <p className="text-green-500">{success}</p>}
+              {error && <p className="text-red-500">{error}</p>}
+            </div>
+          </form>
         </div>
-      <form ref={formRef} onSubmit={handleSubmit}>
-        <div className="md:space-y-4 space-y-3">
-          <input
-            type="text"
-            placeholder="Name"
-            className="bg-black/10 w-full rounded-full border border-white focus:outline-0 focus:border-brianGreen py-5 ps-4 pe-2 placeholder:text-white font-epilogue backdrop-blur-md  placeholder:font-poppins md:placeholder:text-lg md:text-lg "
-            name="name"
-            required
-          />
-          <input
-            type="email"
-            className="w-full rounded-full border border-white focus:outline-0 focus:border-brianGreen py-5 ps-4 pe-2 placeholder:text-white font-epilogue bg-black/10 backdrop-blur-md  placeholder:font-poppins md:placeholder:text-lg md:text-lg "
-            placeholder="Email"
-            name="email"
-            required
-          />
-          <textarea
-            className="w-full rounded-3xl border border-white focus:outline-0 focus:border-brianGreen py-5 ps-4 pe-2 placeholder:text-white h-[150px] font-epilogue bg-black/10 backdrop-blur-md placeholder:font-poppins md:placeholder:text-lg md:text-lg "
-        
-            placeholder="Message"
-            name="message"
-            required
-          ></textarea>
-          <Button
-            className="h-[50px] w-full rounded-full border  border-brianGreen bg-brianGreen font-poppins text-black hover:bg-transparent hover:text-brianGreen max-[320px]:h-fit md:h-[60px] md:w-[250px] 2xl:text-xl"
-            type="submit"
-            disabled={isLoading}
-          >
-            {isLoading ?<Loader className="animate-spin" />  : "Send Message"}
-            
-          </Button>
-          {success && <p className="text-green-500">{success}</p>}
-          {error && <p className="text-red-500">{error}</p>}
-        </div>
-      </form>
-    </div>
-    </section>
+      </section>
     </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -29,16 +29,15 @@ export default function Home() {
 
           <div className="flex flex-col-reverse md:flex-row md:items-center md:justify-between md:gap-10">
             <Link download href="/files/Brianantwi.pdf">
-            <Button className="h-[50px] rounded-full border border-brianGreen bg-transparent font-poppins text-brianGreen hover:bg-brianGreen hover:text-black max-[320px]:h-fit md:h-[70px] md:w-[250px] 2xl:text-xl">
-              Download Résumé
-            </Button>
-
+              <Button className="h-[50px] rounded-full border border-brianGreen bg-transparent font-poppins text-brianGreen hover:bg-brianGreen hover:text-black max-[320px]:h-fit md:h-[70px] md:w-[250px] 2xl:text-xl">
+                Download Résumé
+              </Button>
             </Link>
 
             <div className="hidden lg:block">
               <CircularText />
             </div>
-           
+
             <p className="mb-2 w-full cursor-default font-poppins text-base max-[320px]:mb-1 max-[320px]:text-[14px] md:w-[65%] xl:w-[45%] 2xl:w-[50%] min-[1537px]:w-[45%]">
               Passionate junior Frontend developer dedicated to crafting
               immersive digital experiences. With two years of hands-on
