9afd5068fde3181c563337d10ac53426f4422828
yhawbee1 <antwibrian82@gmail.com>
2025-07-28 13:01:36 +0000

    Update hove style and animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <header className="sticky top-[9px] z-[50] mx-1 flex justify-center">
-      <section className="absolute top-2 w-full rounded-full border border-brianGreen/20 bg-black/40 px-5 py-1.5 backdrop-blur-md lg:w-fit lg:px-10 lg:py-3.5">
+      <section className="absolute top-2 w-full rounded-full border border-brianGreen/20 bg-black/40 px-5 py-1.5 backdrop-blur-md lg:w-fit lg:p-2.5">
         <nav className="flex w-full items-center justify-between lg:justify-around">
           {/* Logo */}
           <Link href={'/'} className="relative size-10 lg:hidden">
@@ -27,11 +27,11 @@ const Header = () => {
           </span>
 
           {/* Navigation links */}
-          <ul className="hidden items-center justify-center gap-10 lg:flex">
+          <ul className="hidden items-center justify-center gap-6 lg:flex">
             {navLinks.map((link, index) => (
               <Link href={link.Link} key={index}>
                 <li
-                  className={`font-poppins transition-colors hover:text-brianGreen hover:line-through`}
+                  className={`font-poppins transition-colors hover:text-black hover:bg-brianGreen px-3.5 py-1.5 rounded-full duration-500`}
                 >
                   {link.Title}
                 </li>
