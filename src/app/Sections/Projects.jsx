f3bdd1f81bab7949fcd076f12517dabbb252a693
yhawbee1 <antwibrian82@gmail.com>
2025-08-01 10:31:32 +0000

    Change grid columns number

--- src/app/Sections/Projects.jsx
+++ src/app/Sections/Projects.jsx
@@ -1,3 +1,4 @@
+import { ExternalLink } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -46,11 +47,11 @@ const Projects = () => {
       <section className="container flex flex-col items-center justify-center">
         <div className="w-full">
           <h2 className="font-epilogue text-[50px] font-bold leading-[50px] md:text-[65px] md:leading-[65px]">
-            Selected <span className='text-brianGreen'>Projects.</span>
+            Selected <span className="text-brianGreen">Projects.</span>
           </h2>
           {/* <p className='font-poppins'>Projects That Define My Growth as a Developer</p> */}
         </div>
-        <div className="mt-4 grid w-full grid-cols-1 gap-2.5 lg:gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-4 grid w-full grid-cols-1 gap-2.5 md:grid-cols-2 md:gap-4 lg:gap-7">
           {projects.map((project, index) => (
             <Card key={index} project={project} />
           ))}
@@ -62,12 +63,11 @@ const Projects = () => {
 
 export default Projects
 
-
 const Card = ({ project }) => {
   return (
-    <a target="_blank" href={project.link}>
+    <a target="_blank" rel="noopener noreferrer" href={project.link}>
       <article
-        className={`relative grid h-[250px] lg:h-[300px] w-full place-items-end overflow-hidden rounded-md border border-gray-700 bg-[url('/${project.image}')] bg-cover`}
+        className={`relative grid h-[250px] w-full overflow-hidden rounded-md items-end border border-gray-700 bg-cover lg:h-[400px]`}
       >
         <Image
           className="absolute h-full w-full object-cover"
@@ -77,10 +77,11 @@ const Card = ({ project }) => {
           priority
           quality={90}
         />
-      </article>
-        <div className="z-10 mb-3 rounded-md p-2 font-poppins">
-          <h3 className="text-lg 2xl:text-2xl">{project.name}</h3>
+        <div className="flex justify-between items-start z-10 mb-3 mx-3 space-y-1.5 rounded-md p-3 font-poppins bg-black/50 backdrop-blur-sm h-fit">
+          <h3 className="text-lg 2xl:text-[20px]">{project.name}</h3>
+          <ExternalLink className=' text-brianGreen' />
         </div>
+      </article>
     </a>
   )
 }
